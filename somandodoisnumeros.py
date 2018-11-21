8e0f3ba6d08bb0dd0ad0cb8e6ecf779c102fb1d2
Raphael <39820693+r84@users.noreply.github.com>
2018-11-21 18:01:32 -0200

    Create somandodoisnumeros.py

---
+++ somandodoisnumeros.py
@@ -0,0 +1,7 @@
+print ("somando dois números")
+
+n1 = int(input("insira o primeiro número:"))
+n2 = int(input("insira o segundo número:"))
+soma = n1 + n2
+
+print ("a soma dos dois número é igual", soma)
