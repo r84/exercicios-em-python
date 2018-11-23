60ef66ba6a916541fd167525596bcf123922b755
Raphael <39820693+r84@users.noreply.github.com>
2018-11-23 02:44:30 -0200

    dobro,triplo e raiz quadrada

---
+++ dobro,triplo e raiz quadrada.py
@@ -0,0 +1,8 @@
+print ("dobro,triplo e raiz quadrada")
+
+n = int(input("insira um número:"))
+dobro = n*2
+triplo = n*3
+raiz = n ** (1/2)
+
+print ("o número", n, "tem seu dobro,", dobro, "o triplo,", triplo, "e sua raiz quadrada é", raiz)
